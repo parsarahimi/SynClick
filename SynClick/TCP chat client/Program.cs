979b5bddb30b0e849f53ab350ea9efafcd21c3a2
parsarahimi <33573025+parsarahimi@users.noreply.github.com>
2020-11-10 11:12:40 +0000

    blah blah

---
+++ SynClick/TCP chat client/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+namespace MultiClient
+{
+
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static string Name;
+        private static IPAddress serverip;
+        private const int PORT = 8000;
+
+        private static bool EnableClick = true;
+        static void BusySleep(double duration)
+        {
+            Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            while (true)
+            {
+                if ((((double)stopwatch.ElapsedTicks) / Stopwatch.Frequency) >= duration)
+                {
+                    return;
+                }
+                //Thread.Sleep(50);
+            }
+        }
+        static void MonitorKeypress()
+        {
+            while (true)
+            {
+                short MonitorME = GetAsyncKeyState(System.Windows.Forms.Keys.XButton1);
+                short MonitorME2 = GetAsyncKeyState(System.Windows.Forms.Keys.XButton2);
+                //Console.WriteLine("MonitorME:" + MonitorME.ToString());
+                if (MonitorME < 0)//when key is pressed its -32768 otherwise 0
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes("shoot");
+                    ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+                    Thread.Sleep(150);
+                }
+                if (MonitorME2 < 0)
+                {
+                    EnableClick = !EnableClick;
+                    if (EnableClick)
+                    {
+                        Console.WriteLine("Click is enabled");
+                        System.Media.SoundPlayer player = new System.Media.SoundPlayer("enabled.wav");
+                        player.Play();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Click is disabled");
+                        System.Media.SoundPlayer player = new System.Media.SoundPlayer("disabled.wav");
+                        player.Play();
+                    }
+                    Thread.Sleep(150);
+                }
+                Thread.Sleep(1);
+            }
+        }
+
+        static double GetTime()
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            long unixTimeStampInTicks;
+            double timestamp;
+            unixTimeStampInTicks = (DateTime.UtcNow - unixStart).Ticks;
+            timestamp = (double)unixTimeStampInTicks / TimeSpan.TicksPerSecond;
+            return timestamp;
+        }
+        static void Main()
+        {
+            Console.Title = "Client";
+            Name = "";
+            while (Name=="")
+            {
+                Console.Write("Enter name: ");
+                Name = Console.ReadLine();
+            }
+            Console.Write("Enter IP: ");
+            serverip = IPAddress.Parse(Console.ReadLine());
+            ConnectToServer();
+            RequestLoop();
+            Exit();
+        }
+
+        private static void ConnectToServer()
+        {
+            int attempts = 0;
+            Stopwatch stopwatch = new Stopwatch();
+            while (!ClientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    Console.WriteLine("Connection attempt " + attempts);
+                    // Change IPAddress.Loopback to a remote IP to connect to a remote host.
+                    stopwatch.Start();
+                    ClientSocket.Connect(serverip, PORT);
+                }
+                catch (SocketException)
+                {
+                    Console.Clear();
+                }
+            }
+            string howlong = ((1000 * (double)stopwatch.ElapsedTicks) / Stopwatch.Frequency).ToString();
+            Console.Clear();
+            Console.WriteLine("ping: "+ howlong + " ms");
+            Console.WriteLine("Open cmd and ping the server again to get a more reliable number, like: ping 127.0.0.1");
+            Console.WriteLine("Connected");
+            Console.WriteLine("Type in ping0.34 for your ping to be set to 340ms, it isn't just that ping though ^ its that plus the ping of the game server, so hold tab in game or whatever to check that too");
+            byte[] buffer = Encoding.ASCII.GetBytes(howlong);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void RequestLoop()
+        {
+            Console.WriteLine(@"<Type ""exit"" to properly disconnect client>");
+            Thread t1 = new Thread(SendRequest);
+            Thread t2 = new Thread(ReceiveResponse);
+            Thread t3 = new Thread(MonitorKeypress);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t1.Join();
+        }
+
+        /// <summary>
+        /// Close socket and exit program.
+        /// </summary>
+        private static void Exit()
+        {
+            SendString("exit"); // Tell the server we are exiting
+            ClientSocket.Shutdown(SocketShutdown.Both);
+            ClientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private static void SendRequest()
+        {
+            while (true)
+            {
+                //Console.Write("Send a request: ");
+            string request = Name+": "+Console.ReadLine();
+            SendString(request);
+            if (request == (Name + ": " + "sync"))
+            {
+                    request = Name + ": " + GetTime().ToString();
+                    Console.WriteLine("myTime: "+ GetTime().ToString());
+                    SendString(request);
+            }
+            if (request == (Name + ": " + "exit"))
+            {
+                Exit();
+            }
+            }
+        }
+
+        /// <summary>
+        /// Sends a string to the server with ASCII encoding.
+        /// </summary>
+        private static void SendString(string text)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(text);
+            ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static void ReceiveResponse()
+        {
+            while (true)
+            {
+                var buffer = new byte[2048];
+                int received = ClientSocket.Receive(buffer, SocketFlags.None);
+                if (received == 0) return;
+                var data = new byte[received];
+                Array.Copy(buffer, data, received);
+                string text = Encoding.ASCII.GetString(data);//fireinmilisec
+                if ((text.IndexOf("fireinmilisec") > -1) && EnableClick)
+                {
+                    double delay = double.Parse(text.Substring(13 + text.IndexOf("fireinmilisec")));
+                    Thread t = new Thread(() => Shoot(delay));
+                    t.Start();
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer("firing.wav");
+                    player.Play();
+                }
+                Console.WriteLine(text);
+            }
+        }
+        private static void Shoot(double howlong)
+        {
+            Console.WriteLine("Recieved fire order " + howlong.ToString()+" seconds");
+            BusySleep(howlong);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+        }
+    }
+}
+//IPAddress ipAddress = IPAddress.Parse("172.21.5.99");
+//IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+//Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+//byte[] buffer = new byte[1024];
+//sender.Send(buffer);
+//int size = sender.Receive(buffer);
+//sender.Close();
---
+++ SynClick/TCP chat/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MultiServer
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private static List<double> clientPings = new List<double>();
+        private static List<bool> clientPremission = new List<bool>();
+        private static string password;
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 8000;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+        static void BusySleep(double duration)
+        {
+            Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+            while (true)
+            {
+                if ((((double)stopwatch.ElapsedTicks) / Stopwatch.Frequency) >= duration)
+                {
+                    return;
+                }
+                //Thread.Sleep(50);
+            }
+        }
+        static double GetTime()
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            long unixTimeStampInTicks;
+            double timestamp;
+            unixTimeStampInTicks = (DateTime.UtcNow - unixStart).Ticks;
+            timestamp = (double)unixTimeStampInTicks / TimeSpan.TicksPerSecond;
+            return timestamp;
+        }
+        static void Main()
+        {
+            Console.Title = "Server";
+            Console.WriteLine("Write password for shoot");
+            password = Console.ReadLine();
+            SetupServer();
+            Console.ReadLine(); // When we press enter close everything
+            CloseAllSockets();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server...");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+            Console.WriteLine("Server setup complete");
+        }
+
+        /// <summary>
+        /// Close all connected client (we do not need to shutdown the server socket as its connections
+        /// are already closed with the clients).
+        /// </summary>
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+
+            serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+
+            try
+            {
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            clientSockets.Add(socket);
+            clientPings.Add(0.0);
+            clientPremission.Add(false);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Console.WriteLine("Client connected, waiting for request...");
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Client forcefully disconnected");
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                current.Close();
+                clientPremission.Remove(clientPremission[clientSockets.IndexOf(current)]);
+                clientPings.Remove(clientPings[clientSockets.IndexOf(current)]);
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+            Console.WriteLine("Received Text: " + text);
+
+            if (text.IndexOf("sync") > -1) // Client requested time
+            {
+                Console.WriteLine("Text is a get time request");
+                byte[] data = Encoding.ASCII.GetBytes("ServerTime: " +GetTime().ToString());//DateTime.Now.ToLongTimeString());
+                //current.Send(data);
+                foreach (Socket socket in clientSockets)
+                {
+                    socket.Send(data);
+                }
+                Console.WriteLine("Time sent to client");
+            }
+            else if (text.IndexOf("ping") > -1) // Client requested time
+            {
+                //Console.WriteLine("Text is a get time request");
+                //byte[] data = Encoding.ASCII.GetBytes("ServerTime: " + GetTime().ToString());//DateTime.Now.ToLongTimeString());
+                //current.Send(data);
+                for (int i=0;i<clientSockets.Count;i++)
+                {
+                    Socket socket = clientSockets[i];
+                    if (socket == current)
+                    {
+                        Console.WriteLine(text.Substring(4+text.IndexOf("ping")));
+                        //Console.ReadLine();
+                        clientPings[i] = double.Parse(text.Substring(4+text.IndexOf("ping")));
+                        Console.WriteLine("Ping recieved to be: "+ clientPings[i].ToString());
+                    }
+
+                }
+                //Console.WriteLine("Time sent to client");
+            }
+            else if (text.IndexOf("shoot") > -1 && clientPremission[clientSockets.IndexOf(current)])
+            {
+                double slowest = clientPings.Max();
+                Console.WriteLine("Shoot recieved with slowest being "+slowest.ToString());
+                for (int i = 0; i < clientSockets.Count; i++)
+                {
+                    Socket socket = clientSockets[i];
+                    byte[] data = Encoding.ASCII.GetBytes("fireinmilisec" + ((slowest-clientPings[i])/2).ToString());
+                    socket.Send(data);
+                }
+            }
+            else if (text.ToLower() == "exit") // Client wants to exit gracefully
+            {
+                // Always Shutdown before closing
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                clientPremission.Remove(clientPremission[clientSockets.IndexOf(current)]);
+                clientPings.Remove(clientPings[clientSockets.IndexOf(current)]);
+                clientSockets.Remove(current);
+                Console.WriteLine("Client disconnected");
+                return;
+            }
+            else if (text.IndexOf(password) > -1) // Client wants to exit gracefully
+            {
+                clientPremission[clientSockets.IndexOf(current)] = true;
+            }
+            else
+            {
+                //Console.WriteLine("Text is an invalid request");
+                //byte[] data = Encoding.ASCII.GetBytes("Invalid request");
+                foreach (Socket socket in clientSockets)
+                {
+                    if (socket != current)
+                    {
+                        socket.Send(recBuf);
+                    }
+                        
+                }
+                    
+                //Console.WriteLine("Warning Sent");
+            }
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+    }
+}
+
+//IPHostEntry host = Dns.GetHostEntry("localhost");
+//IPAddress ipAddress = host.AddressList[0];
+//IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+//Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+//listener.Bind(localEndPoint);
+//listener.Listen(10);
+//Socket handler = listener.Accept();
+//byte[] buffer = new byte[1024];
+//int bytesRec = handler.Receive(buffer);
+//handler.Send(buffer);
+//handler.Shutdown(SocketShutdown.Both);
+//handler.Close();
